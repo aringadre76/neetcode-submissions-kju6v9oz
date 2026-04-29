3bf26639269922e79e52aff23dd4a83795f1de14
aringadre76 <120857324+aringadre76@users.noreply.github.com>
2026-04-28 17:03:15 -0700

    Add: buy-and-sell-crypto - submission-16

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-16.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int l = 0;
+        int r = 1;
+        int profit = 0;
+
+        
+        while (r < prices.Length) {
+            int diff = prices[r]-prices[l];
+            if (diff > 0) {
+                if (diff > profit) {
+                    profit = diff;
+                }
+            }
+            else {
+                l = r;
+            }
+            r++;
+        }
+        return profit;
+    }
+}
