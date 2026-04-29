a6e35e4ffa3ccba05b7f8b971284a9d1190e1396
aringadre76 <120857324+aringadre76@users.noreply.github.com>
2026-04-29 09:52:25 -0700

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public bool IsValid(string s) {
+        var left = new List<char> { '(', '[', '{' };
+
+        var pairs = new Dictionary<char, char>
+        {
+            { ')', '(' },
+            { ']', '[' },
+            { '}', '{' }
+        };
+
+        Stack<char> stack = new Stack<char>();
+
+
+        for (int i = 0; i < s.Length; i++) {
+            char c = s[i];
+
+            if (left.Contains(c) == true) { // current char is left bracket
+                stack.Push(c);
+            } else { // current char is right bracket
+                if (stack.Count != 0) {
+                    
+                    char popped = stack.Pop();
+
+                    if (popped != pairs[c]) {
+                        return false;
+                    }
+
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (stack.Count != 0) {
+            return false;
+        }
+        return true;
+    }
+}
